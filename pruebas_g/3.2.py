c5c5487a59b839aa04e8934741345bcff27bc19b
Jesús Gómez <kachirogomez@hotmail.com>
2020-07-10 17:43:57 -0500

    actual

--- pruebas_g/3.2.py
+++ pruebas_g/3.2.py
@@ -33,7 +33,21 @@ y_eval = dfeval.pop('survived')
 #print(dftrain.head())
 #print(y_train)
 
-plt.show(dftrain.age.hist(bins=20))
+#plt.show(dftrain.age.hist(bins=20))
 #plt.show(dftrain.sex.value_counts().plot(kind='barh'))
 #plt.show(dftrain['class'].value_counts().plot(kind='barh'))
 #plt.show(pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive'))
+
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
+                       'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+  vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
+  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+print(feature_columns)
