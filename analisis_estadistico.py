53f295eea77384e9c35b8385b9894a03322c62f3
David <a00825273@itesm.mx>
2020-07-12 20:52:02 -0500

    correlo pepi

--- analisis_estadistico.py
+++ analisis_estadistico.py
@@ -6,35 +6,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-producto1 = pd.read_csv('tiendaspaketitofroot.csv',delimiter=' ')
+#producto1 = pd.read_csv('tiendaspaketitofroot.csv',delimiter=' ')
 
-print(producto1.head())
-print(producto1.describe(include="all"))
+#print(producto1.head())
+#print(producto1.describe(include="all"))
 
-histo = producto1.rSales.hist(bins=100)
+#histo = producto1.rSales.hist(bins=100)
 
-histo.set_title("Paketito Froot Loops")
+#histo.set_title("Paketito Froot Loops")
 # Set x-axis label
-histo.set_xlabel("Ventas", labelpad=20, size=12)
+#histo.set_xlabel("Ventas", labelpad=20, size=12)
 
 # Set y-axis label
-histo.set_ylabel("Frecuencia", labelpad=20, size=12)
+#histo.set_ylabel("Frecuencia", labelpad=20, size=12)
 
-plt.show()
+#plt.show()
 
-nombres = []
-for col in producto1.columns:
-	nombres.append(col)
+#nombres = []
+#for col in producto1.columns:
+#	nombres.append(col)
 
-print(nombres)
+#print(nombres)
 
 #script_dir = os.getcwd()
 #file = 'AGEBCDMX.csv'
 
-cdmx = pd.read_csv('AGEBCDMX.csv',encoding='latin1',delimiter=' ')
+#cdmx = pd.read_csv('AGEBCDMX.csv',encoding='latin1',delimiter=' ')
 #os.path.normcase(os.path.join(script_dir, file))
-print(cdmx.head())
-print(cdmx.describe(include="all"))
+#print(cdmx.head())
+#print(cdmx.describe(include="all"))
 
 import shapefile
 shape = shapefile.Reader('09a.shp')
